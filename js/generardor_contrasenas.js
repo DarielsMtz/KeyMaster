646445864755f1e4faa29534878acdc84111fd84
DarielsMtz <darielsmartinez926@gmail.com>
2024-02-08 19:50:14 +0100

    Retoques en los estilos .v2

--- js/generardor_contrasenas.js
+++ js/generardor_contrasenas.js
@@ -1,6 +1,6 @@
 // Apartado de la seccion de invitado
 document
-  .getElementById("generar_invitado")
+  .getElementById("invitado__generar")
   .addEventListener("click", function () {
     // Longitud de la contraseña
     const tam_min = 8;
@@ -17,18 +17,32 @@ document
 
     // Arreglo con los caracteres de las contraseña
     let charset = numeros + letras_min + letras_may;
-
     // Generar contraseña aleatoria
     let contrasena = "";
     while (contrasena.length < tamano) {
       const num_aleatorio = Math.floor(Math.random() * charset.length);
       const nuevo_caracter = charset.charAt(num_aleatorio);
-
       if (!contrasena.includes(nuevo_caracter)) {
         contrasena += nuevo_caracter;
       }
     }
 
     // Actualizar el campo de contraseña
-    document.getElementById("password").value = contrasena;
+    document.getElementById("contrasena").value = contrasena;
+  });
+
+// Evento para copiar la contraseña generada
+document
+  .getElementById("invitado__copiar")
+  .addEventListener("click", function () {
+    const contrasena = document.getElementById("contrasena");
+
+    if (contrasena.value) {
+      contrasena.select();
+      document.execCommand("copy");
+      contrasena.blur();
+      alert("Contraseña copiada al portapapeles");
+    } else {
+      alert("Genera una contraseña antes de intentar copiar.");
+    }
   });
