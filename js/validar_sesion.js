e16ee4a87356fc6b465a7914a5612b2605f0a77a
DarielsMtz <127892871+DarielsMtz@users.noreply.github.com>
2024-02-29 11:00:32 +0100

    Actualizar título de página y validar campos vacíos en formulario de inicio de sesión

--- js/validar_sesion.js
+++ js/validar_sesion.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   let formulario = document.getElementById("formulario_sesion");
 
+  // Evento para validar el formulario de inicio de sesión
   formulario.addEventListener("submit", function (event) {
     let usuario = document.getElementById("usuario").value.trim();
     let contrasena = document.getElementById("contrasena").value.trim();
 
-    // Validación de Usuario
+    // Validación de Usuario y Contraseña en caso de estar vacíos
     if (usuario === "" || contrasena === "") {
       alert("Por favor, es necesario rellenar todos los campos.");
       event.preventDefault(); // Evita que el formulario se envíe
